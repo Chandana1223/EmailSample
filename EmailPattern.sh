7ac56e13a6237b789ecd86294172aa3c02fedb43
Chandana1223 <chandana.k232@gmail.com>
2020-12-29 23:33:12 +0530

    Adding valid emailid

--- EmailPattern.sh
+++ EmailPattern.sh
@@ -15,8 +15,18 @@ fi
 
 read -p "enter last name " LastName
 
-Pattern1="^[A-Z]{1}[a-zA-Z]{3,15}$"
+Pattern2="^[A-Z]{1}[a-zA-Z]{3,15}$"
 if [[ $LastName =~ $Pattern1 ]]
+then
+    echo "true"
+else
+    echo "false"
+fi
+
+read -p "enter valid emailid " emailid
+
+Pattern3="^[a-zA-Z0-9]+([._+-][a-zA-Z0-9]+)*@[a-zA-Z0-9]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2,4})?$"
+if [[ $emailid =~ $Pattern3 ]]
 then
     echo "true"
 else
