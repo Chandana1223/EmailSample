bb688ff9d87f9a0915425d431b300edd93b6bb4e
Chandana1223 <chandana.k232@gmail.com>
2020-12-29 22:01:11 +0530

    Adding First name

--- EmailPattern.sh
+++ EmailPattern.sh
@@ -2,4 +2,12 @@
 
 echo "User Registration"
 
+read -p "enter first name " FirstName
 
+Pattern1="^[A-Z]{1}[a-zA-Z]{3,15}$"
+if [[ $FirstName =~ $Pattern1 ]]
+then
+    echo "true"
+else
+    echo "false"
+fi
